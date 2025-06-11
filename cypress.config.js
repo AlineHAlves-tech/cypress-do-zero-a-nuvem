5f7d686697abd6528649436384da052e3071d2bd
AlineHAlves_Tech <aline.barroslp13@gmail.com>
2025-06-10 23:15:32 -0300

    feat: adiciona integração do cypress com Allure via Github Actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
-  e2e: {},
-})
+  e2e: {
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    }
+  }
+});
